631c1b0b2f6b64581fb20da55d1db65f60789e22
Stephen Hodgson <hodgson.designs@gmail.com>
2024-07-04 02:39:06 -0400

    more parallel

--- generate-xref-maps.ps1
+++ generate-xref-maps.ps1
@@ -1,5 +1,11 @@
 # Ensure normalized execution environment
 Set-StrictMode -Version Latest
+
+if ($PSVersionTable.PSVersion.Major -lt 7) {
+    Write-Error "This script requires PowerShell 7.0 or above for parallel processing."
+    exit 1
+}
+
 $ErrorActionPreference = "Stop"
 
 Write-Host "Generating Unity XRef maps"
@@ -32,88 +38,6 @@ if (-not (Test-Path -Path $OutputFolder)) {
     New-Item -ItemType Directory -Path $OutputFolder -Force
 }
 
-function normalizeText {
-    param (
-        [string]$text
-    )
-    if ($null -ne $text -match '(\(|<)') { $text = $text.Split('(<)')[0] }
-    $text = $text -replace '[`]', '_' -replace '#ctor', 'ctor'
-    return $text
-}
-
-function validateUrl {
-    param (
-        [string]$url
-    )
-    try {
-        $response = Invoke-WebRequest -Uri $url -Method Head -ErrorAction Stop
-        return $response.StatusCode -eq 200
-    }
-    catch {
-        return $false
-    }
-}
-
-function rewriteHref {
-    param (
-        [string]$uid,
-        [string]$commentId,
-        [string]$version
-    )
-
-    $href = $uid
-    $altHref = $null
-
-    $nsTrimRegex = [regex]::new("^UnityEngine\.|^UnityEditor\.")
-
-    if ($commentId -match "^N:") {
-        $href = "index"
-    }
-    else {
-        $href = $nsTrimRegex.Replace($href, "")
-
-        if ($commentId -match "^F:.*") {
-            $isEnum = $href -match "\.([a-zA-Z][a-zA-Z0-9_]*)$"
-            if ($isEnum -and $Matches[1] -cmatch "^[a-z]") {
-                $href = $href -replace "\.$($Matches[1])$", "-$($Matches[1])"
-            }
-        }
-        elseif ($commentId -match "^M:.*\.#ctor$") {
-            $href = $href -replace "\.\#ctor$", "-ctor"
-        }
-        else {
-            $href = $href -replace "``\d", "" -replace '`', ""
-
-            if ($commentId -match "^M:" -or $commentId -match "^(P|E):" -and $href.LastIndexOf('.') -ne -1) {
-                $href = $href.Substring(0, $href.LastIndexOf('.')) + "-" + $href.Substring($href.LastIndexOf('.') + 1)
-            }
-        }
-    }
-
-    $url = "https://docs.unity3d.com/$version/Documentation/ScriptReference/$href.html"
-
-    if (validateUrl -url $url) {
-        return $url
-    }
-    else {
-        if ($href -match "-") {
-            $altHref = $href -replace "-", "."
-        }
-        else {
-            $altHref = $href -replace "\.", "-"
-        }
-
-        $altUrl = "https://docs.unity3d.com/$version/Documentation/ScriptReference/$altHref.html"
-
-        if (validateUrl -url $altUrl) {
-            return $altUrl
-        }
-        else {
-            Write-Warning "$uid -> $url"
-            return $null
-        }
-    }
-}
 
 # Index HTML initial content
 Set-Content -Path "$OutputFolder/index.html" -Value "<html><body><ul>"
@@ -137,6 +61,8 @@ catch {
 try {
     $branches = $branchesOutput | Select-String -Pattern 'origin/\d{4}\.\d+$' | ForEach-Object {
         $_.Matches.Value.Trim()
+    } | ForEach-Object {
+        $_ -replace 'origin/', ''
     }
 
     if (-not $branches) {
@@ -149,15 +75,17 @@ catch {
     exit 1
 }
 
+$metadataList = @()
+
 foreach ($branch in $branches) {
     # Parse version from branch name
-    if ($branch -match 'origin/(\d{4}\.\d+)') {
-        $version = $Matches[1]
+    if ($branch -match '\d{4}\.\d+') {
+        $version = $Matches[0]
 
         Write-Host "Processing version: $version"
 
         try {
-            git -C $UnityCsReferenceLocalPath checkout --force $branch
+            git -C $UnityCsReferenceLocalPath checkout --force "origin/$branch"
             git -C $UnityCsReferenceLocalPath reset --hard
             git -C $UnityCsReferenceLocalPath clean -ffdx
         }
@@ -169,73 +97,172 @@ foreach ($branch in $branches) {
         $DocfxPath = Join-Path $DocfxLocalDir "docfx.json"
         $versionFolder = Join-Path $DocfxLocalDir "api/$version"
 
-        Write-Host "Generating docfx metadata for version $version using $DocfxPath in $versionFolder"
-        docfx metadata $DocfxPath --output $versionFolder --logLevel diagnostic
+        # if the version folder already exists, skip metadata generation but add to the list
 
-        if ($LASTEXITCODE -ne 0) {
-            Write-Error "DocFX metadata generation failed for $version"
-            continue
+        if (-not (Test-Path -Path $versionFolder)) {
+            Write-Host "Generating docfx metadata for version $version using $DocfxPath in $versionFolder"
+            docfx metadata $DocfxPath --output $versionFolder
+
+            if ($LASTEXITCODE -ne 0) {
+                Write-Error "DocFX metadata generation failed for $version"
+                continue
+            }
         }
 
-        # Path to generated metadata
-        $GeneratedMetadataPath = Join-Path $PWD ".docfx/api/$version"
+        $metadataList += [PSCustomObject]@{
+            Version               = $version
+            GeneratedMetadataPath = Join-Path $DocfxLocalDir "api/$version"
+        }
+    }
+}
 
-        # Generate XRef map YAML
-        Write-Host "Generating XRef map for version $version"
-        $references = @()
-        $files = Get-ChildItem -Path $GeneratedMetadataPath -Filter '*.yml'
+# Use parallel processing to handle the XRef map generation
+Write-Host "Processing XRef maps in parallel..."
 
-        foreach ($file in $files) {
-            $filePath = $file.FullName
-            $firstLine = Get-Content $filePath -TotalCount 1
-            if ($firstLine -eq "### YamlMime:ManagedReference") {
-                $yaml = Get-Content $filePath -Raw | ConvertFrom-Yaml
-                $items = $yaml.items
+$metadataList | ForEach-Object -Parallel {
+    $version = $_.Version
+    $GeneratedMetadataPath = $_.GeneratedMetadataPath
+    $outputFolder = $using:OutputFolder
 
-                foreach ($item in $items) {
-                    try {
-                        $fullName = normalizeText $item.fullName
-                        $name = normalizeText $item.name
-                        $href = rewriteHref -uid $item.uid -commentId $item.commentId -version $version
+    Write-Host "Generating XRef map for version $version"
+    $references = @()
+    $files = Get-ChildItem -Path $GeneratedMetadataPath -Filter '*.yml'
+    $references += $files | ForEach-Object -Parallel {
+        function normalizeText {
+            param (
+                [string]$text
+            )
+            if ($null -ne $text -match '(\(|<)') { $text = $text.Split('(<)')[0] }
+            $text = $text -replace '[`]', '_' -replace '#ctor', 'ctor'
+            return $text
+        }
 
-                        if ($null -ne $href) {
-                            # Write-Host "$fullName -> $href"
+        function validateUrl {
+            param (
+                [string]$url
+            )
+            try {
+                $response = Invoke-WebRequest -Uri $url -Method Head -ErrorAction Stop
+                return $response.StatusCode -eq 200
+            }
+            catch {
+                return $false
+            }
+        }
 
-                            $references += [PSCustomObject]@{
-                                uid          = $item.uid
-                                name         = $name
-                                href         = $href
-                                commentId    = $item.commentId
-                                fullName     = $fullName
-                                nameWithType = $item.nameWithType
-                            }
-                        }
+        function rewriteHref {
+            param (
+                [string]$uid,
+                [string]$commentId,
+                [string]$version
+            )
+
+            $href = $uid
+            $altHref = $null
+
+            $nsTrimRegex = [regex]::new("^UnityEngine\.|^UnityEditor\.")
+
+            if ($commentId -match "^N:") {
+                $href = "index"
+            }
+            else {
+                $href = $nsTrimRegex.Replace($href, "")
+
+                if ($commentId -match "^F:.*") {
+                    $isEnum = $href -match "\.([a-zA-Z][a-zA-Z0-9_]*)$"
+                    if ($isEnum -and $Matches[1] -cmatch "^[a-z]") {
+                        $href = $href -replace "\.$($Matches[1])$", "-$($Matches[1])"
                     }
-                    catch {
-                        Write-Error "Error processing item: $item `nDetails: $_"
+                }
+                elseif ($commentId -match "^M:.*\.#ctor$") {
+                    $href = $href -replace "\.\#ctor$", "-ctor"
+                }
+                else {
+                    $href = $href -replace "``\d", "" -replace '`', ""
+
+                    if ($commentId -match "^M:" -or $commentId -match "^(P|E):" -and $href.LastIndexOf('.') -ne -1) {
+                        $href = $href.Substring(0, $href.LastIndexOf('.')) + "-" + $href.Substring($href.LastIndexOf('.') + 1)
                     }
+                }
+            }
+
+            $url = "https://docs.unity3d.com/$version/Documentation/ScriptReference/$href.html"
+
+            if (validateUrl -url $url) {
+                return $url
+            }
+            else {
+                if ($href -match "-") {
+                    $altHref = $href -replace "-", "."
+                }
+                else {
+                    $altHref = $href -replace "\.", "-"
+                }
+
+                $altUrl = "https://docs.unity3d.com/$version/Documentation/ScriptReference/$altHref.html"
+
+                if (validateUrl -url $altUrl) {
+                    return $altUrl
+                }
+                else {
+                    Write-Warning "$uid -> $url"
+                    return $null
                 }
             }
         }
 
-        Write-Host "Sorting references"
+        $filePath = $_.FullName
+        $referencesLocal = @()
 
-        $xrefMapContent = @{
-            "### YamlMime:XRefMap" = $null
-            sorted                 = $true
-            references             = $references | Sort-Object Uid
-        } | ConvertTo-Yaml
+        $firstLine = Get-Content $filePath -TotalCount 1
+        if ($firstLine -eq "### YamlMime:ManagedReference") {
+            $yaml = Get-Content $filePath -Raw | ConvertFrom-Yaml
+            $items = $yaml.items
 
-        $relativeOutputFilePath = "$version/xrefmap.yml"
-        $outputFilePath = Join-Path $OutputFolder $relativeOutputFilePath
+            foreach ($item in $items) {
+                try {
+                    $fullName = normalizeText $item.fullName
+                    $name = normalizeText $item.name
+                    $href = rewriteHref -uid $item.uid -commentId $item.commentId -version $version
 
-        Write-Host "Writing XRef map to $outputFilePath"
-        New-Item -ItemType Directory -Path (Split-Path $outputFilePath) -Force
-        Set-Content -Path $outputFilePath -Value $xrefMapContent
-        Add-Content -Path "$OutputFolder/index.html" -Value "<li><a href=""$relativeOutputFilePath"">$version</a></li>"
-    }
-}
+                    if ($null -ne $href) {
+                        Write-Host "$fullName -> $href"
+                        $referencesLocal += [PSCustomObject]@{
+                            uid          = $item.uid
+                            name         = $name
+                            href         = $href
+                            commentId    = $item.commentId
+                            fullName     = $fullName
+                            nameWithType = $item.nameWithType
+                        }
+                    }
+                }
+                catch {
+                    Write-Error "Error processing item: $item `nDetails: $_"
+                }
+            }
+        }
 
-Add-Content -Path "$OutputFolder/index.html" -Value "</ul></body></html>"
+        return $referencesLocal
+    } -ThrottleLimit 100
 
+    Write-Host "$version Sorting references"
+
+    $xrefMapContent = @{
+        "### YamlMime:XRefMap" = $null
+        sorted                 = $true
+        references             = $references | Sort-Object Uid
+    } | ConvertTo-Yaml
+
+    $relativeOutputFilePath = "$version/xrefmap.yml"
+    $outputFilePath = Join-Path $outputFolder $relativeOutputFilePath
+
+    Write-Host "$version Writing XRef map to $outputFilePath"
+    New-Item -ItemType Directory -Path (Split-Path $outputFilePath) -Force
+    Set-Content -Path $outputFolder -Value $xrefMapContent
+    Add-Content -Path "$outputFolder/index.html" -Value "<li><a href=""$relativeOutputFilePath"">$version</a></li>"
+
+} -ArgumentList $_, $outputFolder -ThrottleLimit 5
+
+Add-Content -Path "$outputFolder/index.html" -Value "</ul></body></html>"
 Write-Host "Unity XRef maps generated successfully!"
