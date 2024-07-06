0987c348afd3dce192dee459d6fd66a45218295b
Stephen Hodgson <hodgson.designs@gmail.com>
2024-07-06 17:18:31 -0400

    reorder

--- generate-xref-map.sh
+++ generate-xref-map.sh
@@ -17,46 +17,11 @@ files=$(find "$generatedMetadataPath" -name '*.yml')
 
 echo "Generating XRef map for Unity $version"
 
-for file in $files; do
-    firstLine=$(head -n 1 "$file")
-    if [[ "$firstLine" == "### YamlMime:ManagedReference" ]]; then
-        echo "Processing file: $file"
-
-        items=$(tail -n +1 "$file" | yq -r '.items[]')
-
-        # Debugging: Check if items are properly retrieved
-        if [[ -z "$items" ]]; then
-            echo "No items found in the YAML content for file $file"
-            continue
-        fi
-
-        while IFS= read -r item; do
-            # Debugging: Print the current item being processed
-            echo "Processing item: $item"
-
-            fullName=$(echo "$item" | yq -r '.fullName' | sed 's/[()<].*//g' | sed 's/`/_/g' | sed 's/#ctor/ctor/g')
-            name=$(echo "$item" | yq -r '.name' | sed 's/[()<].*//g' | sed 's/`/_/g' | sed 's/#ctor/ctor/g')
-            uid=$(echo "$item" | yq -r '.uid')
-            commentId=$(echo "$item" | yq -r '.commentId')
-            href=$(rewriteHref "$uid" "$commentId" "$version")
-
-            if [ -n "$href" ]; then
-                references+=("{\"uid\": \"$uid\", \"name\": \"$name\", \"href\": \"$href\", \"commentId\": \"$commentId\", \"fullName\": \"$fullName\", \"nameWithType\": \"$(echo "$item" | yq -r '.nameWithType')\"}")
-                echo "$fullName -> $href"
-            else
-                echo "Failed to process item: $item"
-            fi
-        done <<<"$items"
-    fi
-done
-
-# Convert references to YAML
-referencesYaml=$(printf "%s\n" "${references[@]}" | jq -s '.' | yq -P)
-xrefMapContent=$(yq -n --arg references "$referencesYaml" "{ \"### YamlMime:XRefMap\": null, \"sorted\": true, \"references\": \$references | sort_by(.uid) }")
-outputFilePath="$outputFolder/$version/xrefmap.yml"
-mkdir -p "$(dirname "$outputFilePath")"
-echo "$xrefMapContent" >"$outputFilePath"
-echo "Unity $version XRef Map generated successfully!"
+validateUrl() {
+    url=$1
+    httpCode=$(curl -o /dev/null -s -w "%{http_code}\n" -I "$url")
+    [[ $httpCode -eq 200 ]]
+}
 
 rewriteHref() {
     uid=$1
@@ -104,8 +69,42 @@ rewriteHref() {
     fi
 }
 
-validateUrl() {
-    url=$1
-    httpCode=$(curl -o /dev/null -s -w "%{http_code}\n" -I "$url")
-    [[ $httpCode -eq 200 ]]
-}
+for file in $files; do
+    firstLine=$(head -n 1 "$file")
+    if [[ "$firstLine" == "### YamlMime:ManagedReference" ]]; then
+        echo "Processing file: $file"
+        items=$(tail -n +1 "$file" | yq -r '.items[]')
+
+        # Debugging: Check if items are properly retrieved
+        if [[ -z "$items" ]]; then
+            echo "No items found in the YAML content for file $file"
+            continue
+        fi
+
+        while IFS= read -r item; do
+            # Debugging: Print the current item being processed
+            echo "Processing item: $item"
+
+            fullName=$(echo "$item" | yq -r '.fullName' | sed 's/[()<].*//g' | sed 's/`/_/g' | sed 's/#ctor/ctor/g')
+            name=$(echo "$item" | yq -r '.name' | sed 's/[()<].*//g' | sed 's/`/_/g' | sed 's/#ctor/ctor/g')
+            uid=$(echo "$item" | yq -r '.uid')
+            commentId=$(echo "$item" | yq -r '.commentId')
+            href=$(rewriteHref "$uid" "$commentId" "$version")
+
+            if [ -n "$href" ]; then
+                references+=("{\"uid\": \"$uid\", \"name\": \"$name\", \"href\": \"$href\", \"commentId\": \"$commentId\", \"fullName\": \"$fullName\", \"nameWithType\": \"$(echo "$item" | yq -r '.nameWithType')\"}")
+                echo "$fullName -> $href"
+            else
+                echo "Failed to process item: $item"
+            fi
+        done <<<"$items"
+    fi
+done
+
+# Convert references to YAML
+referencesYaml=$(printf "%s\n" "${references[@]}" | jq -s '.' | yq -P)
+xrefMapContent=$(yq -n --arg references "$referencesYaml" "{ \"### YamlMime:XRefMap\": null, \"sorted\": true, \"references\": \$references | sort_by(.uid) }")
+outputFilePath="$outputFolder/$version/xrefmap.yml"
+mkdir -p "$(dirname "$outputFilePath")"
+echo "$xrefMapContent" >"$outputFilePath"
+echo "Unity $version XRef Map generated successfully!"
