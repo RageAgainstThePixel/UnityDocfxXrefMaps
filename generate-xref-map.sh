53ee6901874877f423ac17465fd11a0e28124144
Stephen Hodgson <hodgson.designs@gmail.com>
2024-07-07 18:13:09 -0400

    debug

--- generate-xref-map.sh
+++ generate-xref-map.sh
@@ -12,6 +12,7 @@ function normalize_text {
 
 function validate_url {
     local url="$1"
+    echo "Validating $url"
     status_code=$(curl --head --silent --output /dev/null --write-out "%{http_code}" "$url")
     [[ "$status_code" -eq 200 ]]
 }
@@ -21,16 +22,21 @@ function rewrite_href {
     local comment_id="$2"
     local version="$3"
 
+    echo "Processing [$uid] $comment_id"
+
     local href="$uid"
     local alt_href=""
 
     # Trim namespaces
     href=$(echo "$href" | sed -E 's/^UnityEngine\.|^UnityEditor\.//g')
-
     if [[ "$comment_id" =~ ^N: ]]; then
         href="index"
     else
-        if [[ "$comment_id" =~ ^F:.* ]]; then
+        href="${href//$()[0-9]/}" # Remove sequences like ``2
+        href="${href//\`/}"       # Remove backticks
+
+        if [[ "$comment_id" =~ ^F: ]]; then
+            # Field case
             if [[ "$href" =~ \.([a-zA-Z][a-zA-Z0-9_]*)$ ]]; then
                 local last_part="${BASH_REMATCH[1]}"
                 if [[ "$last_part" =~ ^[a-z] ]]; then
@@ -38,22 +44,26 @@ function rewrite_href {
                 fi
             fi
         elif [[ "$comment_id" =~ ^M:.*\.#ctor$ ]]; then
+            # Constructor case
             href="${href//\.#ctor/-ctor}"
+        elif [[ "$comment_id" =~ ^M: ]]; then
+            # Method case
+            if [[ "$href" =~ \.(.*)$ ]]; then
+                href="${href%.*}-${BASH_REMATCH[1]}"
+            fi
+        elif [[ "$comment_id" =~ ^P: ]]; then
+            # Property case
+            if [[ "$href" =~ \.(.*)$ ]]; then
+                href="${href%.*}-${BASH_REMATCH[1]}"
+            fi
         else
-            # This handles properties and methods
-            href="${href//$()[0-9]/}" # Remove sequences like ``2
-            href="${href//\`/}"       # Remove backticks
-
-            if [[ ("$comment_id" =~ ^M:.*) || ("$comment_id" =~ ^P:.*) || ("$comment_id" =~ ^E:.*) ]]; then
-                if [[ "$href" =~ \. ]]; then
-                    href="${href%.*}-${href##*.}"
-                fi
+            # General cases
+            if [[ "$href" =~ \.(.*)$ ]]; then
+                href="${href%.*}-${BASH_REMATCH[1]}"
             fi
         fi
     fi
-
     local url="https://docs.unity3d.com/$version/Documentation/ScriptReference/$href.html"
-
     if validate_url "$url"; then
         echo "$url"
     else
@@ -77,30 +87,23 @@ function generate_xref_map {
     local output_folder="$3"
     references=()
     processed_uids=()
-
     # Iterate over every YAML file in the generated metadata path
     for file in "$generated_metadata_path"/*.yml; do
-        yq --exit-status 'tag == "!!map" or tag== "!!seq"' "$file" >/dev/null
         # Validate if the file contains "### YamlMime:ManagedReference" on the first line
         first_line=$(head -n 1 "$file")
-
         if [[ "$first_line" == "### YamlMime:ManagedReference" ]]; then
             echo "Processing $file"
             readarray items < <(yq eval -o=j -I=0 '.items[]' "$file")
-
             if [[ ${#items[@]} -eq 0 ]]; then
                 echo "No items found in $file"
                 continue
             fi
-
             for item in "${items[@]}"; do
                 uid=$(echo "$item" | yq '.uid')
-
                 # Skip if the uid has already been processed
                 if [[ " ${processed_uids[*]} " =~ ${uid} ]]; then
                     continue
                 fi
-
                 full_name=$(normalize_text "$(echo "$item" | yq '.fullName')")
                 name=$(normalize_text "$(echo "$item" | yq '.name')")
                 comment_id=$(echo "$item" | yq '.commentId')
@@ -122,17 +125,14 @@ function generate_xref_map {
             done
         fi
     done
-
     # Compile all references data into the final output YAML
     xref_map_content=$(jq -n \
         --argjson references "$(printf '%s\n' "${references[@]}" | jq -s '.')" \
         '{ "### YamlMime:XRefMap": null, sorted: true, references: $references }')
-
     # Generate the output file
     local output_file_path="$output_folder/$version/xrefmap.yml"
     mkdir -p "$(dirname "$output_file_path")"
     echo "$xref_map_content" | yq eval -P >"$output_file_path"
-
     echo "Unity $version XRef Map generated successfully!"
 }
 
